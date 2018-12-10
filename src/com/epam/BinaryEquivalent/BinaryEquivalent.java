4904e64c35560e3bbafc4789bd66c59c27674a1a
baural <alzhanovbb@gmail.com>
2018-12-11 05:45:18 +0600

    first draft

---
+++ src/com/epam/BinaryEquivalent/BinaryEquivalent.java
@@ -0,0 +1,10 @@
+package com.epam.BinaryEquivalent;
+
+public class BinaryEquivalent {
+    private static final String First = "10100101";
+    private static final String Second = "11001001";
+
+    public static void main(String[] args) {
+
+    }
+}
